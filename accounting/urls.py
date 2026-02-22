66f52386b194297da2977c94d3b4c844657ad8df
bobjiangps <jbsv43@sina.com>
2026-02-22 18:38:32 +0800

    update setting

--- accounting/urls.py
+++ accounting/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index_accounting'),
     path('retrieve_category/', views.retrieve_category, name='retrieve_category'),
     path('retrieve_subcategory/', views.retrieve_subcategory, name='retrieve_subcategory'),
     path('record_income_expense/', views.record_income_expense, name='record_income_expense'),
