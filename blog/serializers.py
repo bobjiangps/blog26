2fcc794ed34d0806d395210027ecd49eb3997164
bobjiangps <jbsv43@sina.com>
2026-03-21 10:23:48 +0800

    add new attributes for bleach to clean then show to page

--- blog/serializers.py
+++ blog/serializers.py
@@ -16,11 +16,23 @@ class PostSerializer(serializers.ModelSerializer):
         make sure html content is safe by using bleach to sanitize it
         to avoid XSS attacks. Only allow a limited set of tags and attributes.
         """
-        allowed_tags = ['p', 'div', 'span', 'br', 'h1', 'h2', 'h3', 'code', 'pre', 'img', 'table', 'tr', 'td', 'th', 'a', 'strong', 'em']
+        # allowed_tags = ['p', 'div', 'span', 'br', 'h1', 'h2', 'h3', 'code', 'pre', 'img', 'table', 'tr', 'td', 'th', 'a', 'strong', 'em']
+        allowed_tags = [
+            'p', 'div', 'span', 'br',
+            'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+            'ul', 'ol', 'li',  # 新增：列表标签
+            'blockquote', 'pre', 'code',  # 新增：引用和代码
+            'hr',  # 新增：分割线
+            'strong', 'em', 's', 'del', 'u',  # 新增：常见格式
+            'img', 'a',
+            'table', 'thead', 'tbody', 'tr', 'th', 'td',  # 表格
+            'input'  # 新增：用于 Vditor 的任务列表复选框
+        ]
         allowed_attrs = {
-            '*': ['class', 'style'],
-            'a': ['href', 'target'],
-            'img': ['src', 'alt']
+            '*': ['class', 'style','id', 'title'],   # 放宽：允许大部分标签使用 class（代码高亮需要）
+            'a': ['href', 'target', 'rel', 'title'],
+            'img': ['src', 'alt', 'title', 'width', 'height'],
+            'input': ['type', 'checked', 'disabled'] # 任务列表支持
         }
         allowed_styles = ['color', 'font-weight', 'font-size', 'background-color', 'text-align', 'width', 'height']
         css_sanitizer = CSSSanitizer(allowed_css_properties=allowed_styles)
